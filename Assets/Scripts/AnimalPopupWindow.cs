e8803262fe7b99c203ef8f17100414eb15c2509b
a.baranov <a.baranov@overmindgroup.com>
2022-11-19 15:45:19 +0300

    feat: добавлен заголовок окна

---
+++ Assets/Scripts/AnimalPopupWindow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace UnityEditorInternal
+{
+    public class AnimalPopupWindow : MonoBehaviour
+    {
+
+        /// <summary>
+        /// Т.к. я не знаю, как часто используется это окно, при закрытии оно будет именно уничтожаться.
+        /// </summary>
+        public void CloseWindow()
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
